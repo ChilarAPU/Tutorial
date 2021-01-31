3bb5173a55281cbe068550b460370f520c0f452e
ChilarAPU <61912039+ChilarAPU@users.noreply.github.com>
2021-01-31 00:17:22 +0000

    Add files via upload

--- Tutorial Files/Code/SceneA.js
+++ Tutorial Files/Code/SceneA.js
@@ -1,5 +1,7 @@
 class SceneA extends Phaser.Scene {
     map;
+    player;
+    playerStartPoint;
     preload() {
         //loading in the assets (bat and heart)
         this.load.image('bat', '../Assets/bat.png');
@@ -22,11 +24,40 @@ class SceneA extends Phaser.Scene {
         this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
         //finding and adding the tileset from Tiled 
         let tiles = this.map.addTilesetImage('Medieval-Tileset', 'tiles');
-        //creating background layer from Tiled map
+        //creating layers from Tiled map
         this.map.createStaticLayer('background', [tiles], 0, 0);
+        let groundLayer = this.map.createStaticLayer('ground', [tiles], 0, 0);
+        groundLayer.setCollisionBetween(1, 1000);
+
+        //creating the player and spawning the player in the point made in Tiled
+        this.playerStartPoint = SceneA.FindPoint(this.map, 'object', 'player', 'playerSpawn');
+        this.player = this.physics.add.sprite(this.playerStartPoint.x, this.playerStartPoint.y, 'player');
+        //resizing the players hitbox
+        this.player.body.setSize(40, 60, true);
+        //moving the players hitbox so the player is centered
+        this.player.body.setOffset(25, 0);
+        this.player.setCollideWorldBounds(true);
+        this.physics.add.collider(this.player, groundLayer);
     }
 
     update() {
 
     }
+
+    static FindPoint(map, layer, type, name) {
+        var loc = map.findObject(layer, function (object) {
+          if (object.type === type && object.name === name) {
+            return object;
+          }
+        });
+        return loc
+      }
+      static FindPoints(map, layer, type) {
+        var locs = map.filterObjects(layer, function (object) {
+          if (object.type === type) {
+            return object
+          }
+        });
+        return locs
+      }
 }
